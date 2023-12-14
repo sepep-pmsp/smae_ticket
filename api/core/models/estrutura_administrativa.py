92f06658f649e50d002052ba02a1a04acbcb54d7
hpgy <hpougy@gmail.com>
2023-12-14 13:00:15 -0300

    modelos iniciais - user e orgao

---
+++ api/core/models/estrutura_administrativa.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import List, Optional
+
+from .database import Base
+#evitando import circular
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .users import User
+
+
+
+class TipoOrgao(Base):
+    __tablename__ = "tipos_orgao"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tipo : Mapped[str] = mapped_column(unique=True)
+    desc : Mapped[str]
+    is_ativo : Mapped[bool]
+    orgaos: Mapped[List["Orgao"]] = relationship()
+    
+class Orgao(Base):
+    __tablename__ = "orgaos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sigla : Mapped[str] = mapped_column(String(10))
+    nome : Mapped[str]
+    is_ativo : Mapped[bool]
+    tipo_orgao_id: Mapped[int] = mapped_column(ForeignKey("tipos_orgao.id"))
+    tipo_orgao: Mapped["TipoOrgao"] = relationship(back_populates="orgaos")
+    usuarios : Mapped[List["User"]] = relationship(back_populates='orgao')
+
+
---
+++ api/core/models/users.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from typing import List, Optional
+
+from .database import Base
+from .estrutura_administrativa import Orgao
+
+#evitando import circular
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .estrutura_administrativa import Orgao
+
+user_perfil = Table(
+    "user_perfil",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id")),
+    Column("perfil_id", ForeignKey("perfis.id")),
+)
+
+
+perfil_permissao = Table(
+    "perfil_permissao",
+    Base.metadata,
+    Column("perfil_id", ForeignKey("perfis.id")),
+    Column("permissao_id", ForeignKey("permissoes.id")),
+)
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email : Mapped[str] = mapped_column(unique=True)
+    nome : Mapped[str]
+    hashed_password : Mapped[str]
+    is_ativo : Mapped[bool]
+    
+    orgao_id: Mapped[int] = mapped_column(ForeignKey("orgaos.id"))
+    orgao: Mapped[Orgao] = relationship(back_populates="usuarios")
+    perfis: Mapped[List["Perfil"]] = relationship(secondary=user_perfil)
+
+class Perfil(Base):
+    __tablename__ = "perfis"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title :  Mapped[str] = mapped_column()
+    description : Mapped[str] = mapped_column()
+
+    permissoes: Mapped[List["Permissao"]] = relationship(secondary=perfil_permissao)
+
+
+class Permissao(Base):
+    __tablename__ = "permissoes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
